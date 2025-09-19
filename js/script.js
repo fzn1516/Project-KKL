7907056cd3222bdeac508444d6063e47eae88f52
fzn1516 <enisuwartinah88@guru.sma.belajar.id>
2025-09-19 14:21:35 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function goLevel(id){
   localStorage.setItem('levelId', id); // simpan level yang dipilih
-  window.location.href = '../icon.html'; // pindah ke halaman ikon
+  window.location.href = 'https://fzn1516.github.io/Project-KKL-main-/'; // pindah ke halaman ikon
 }
+
 
